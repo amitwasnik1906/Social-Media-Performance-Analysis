baaa8e0ddcfebbfe298d95aa7a8c04b8e3f52893
Amit <amitwasnik2004@gmail.com>
2025-01-05 12:35:19 +0530

    test deploy

--- src/components/GraphCard.tsx
+++ src/components/GraphCard.tsx
@@ -1,18 +1,7 @@
 'use client';
-/* eslint-disable no-use-before-define */
 
 import React from 'react';
 
-interface PostData {
-    type: string;
-    likes: number;
-    comments: number;
-    shares: number;
-}
-
-interface GraphCardProps {
-    postData: PostData[];
-}
 
 export default function GraphCard() {
 
