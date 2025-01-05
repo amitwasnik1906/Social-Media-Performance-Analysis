d621e3246d61e47ba65d915ddced93dfa49263c7
Amit <amitwasnik2004@gmail.com>
2025-01-05 16:41:09 +0530

    langflow request added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,21 +5,11 @@ import Header from "@/components/Header";
 import { fetchData } from "@/lib/fetchAction";
 
 export default async function Home() {
-  // const res = await fetchData("Analyze the provided social media engagement data and return a concise summary with average likes, comments, and shares for each post type (e.g., carousel, reels, static images). Additionally, provide a brief insight into the best-performing post type and a short recommendation for optimizing engagement. Ensure the output is suitable for direct visualization in graphs")
+  const res = await fetchData("Analyze the provided social media engagement data and return a concise summary with average likes, comments, and shares for each post type (e.g., carousel, reels, static images). Additionally, provide a brief insight into the best-performing post type and a short recommendation for optimizing engagement. Ensure the output is suitable for direct visualization in graphs")
 
-  // const output = res.outputs[0].outputs[0].artifacts?.message
-  // console.log(output)
-  // const text = output
-
-  await new Promise(resolve => setTimeout(resolve, 4000))
-  const text = `
-    Post Type,Average Likes,Average Comments,Average Shares
-    Carousel,128.24,62.44,43.64
-    Reel,107.11,55.67,34.44
-    Static_Image,51.12,27.24,16.00
-    Insight: Carousels have the highest average engagement across all metrics (likes, comments, shares), significantly outperforming other post types. Reels also perform well, especially in terms of likes and comments, exceeding static images.
-    Recommendation: Focus on creating more carousel posts to maximize overall engagement. Experiment with interactive elements within carousels (e.g., polls, quizzes) to further boost interaction. While Reels show strong performance, explore incorporating calls to action to potentially improve share counts. Static images could benefit from more engaging visuals and captions to increase interaction.
-  `;
+  const output = res.outputs[0].outputs[0].artifacts?.message
+  console.log(output)
+  const text = output
 
   return (
     <div>
